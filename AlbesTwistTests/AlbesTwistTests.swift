6654fbdfd209d789ea0dc595329186fdbac00451
Alberto Guarino <gitpara@gmail.com>
2018-01-06 17:03:09 +0100

    Initial import

---
+++ AlbesTwistTests/AlbesTwistTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import AlbesTwist
+
+class AlbesTwistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
