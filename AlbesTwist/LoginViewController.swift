057f1a10a715e39c4667f86825249a0299b28d52
Alberto Guarino <gitpara@gmail.com>
2018-01-07 17:02:29 +0100

    LoginViewController skeleton and basic UI.

---
+++ AlbesTwist/LoginViewController.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func loginButtonPushed(_ sender: Any) {
+        
+    }
+
+}
+
